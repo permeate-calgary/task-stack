c47a6fa9e1fbdd613d5bd603491dcdadc805a030
daxi <aljedaxi@gmail.com>
2020-01-21 20:47:01 -0700

    Updates

---
+++ precache-manifest.8001377c6a7476f0db164f363e42dceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dcbef324ec52f59a8fb87e3c5354ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1af05b1c7e617d23f96",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "007dac5dcbc82d408cef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d1af05b1c7e617d23f96",
+    "url": "/static/js/2.6b6bb41e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.6b6bb41e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "007dac5dcbc82d408cef",
+    "url": "/static/js/main.85d5497c.chunk.js"
+  },
+  {
+    "revision": "7044b765e8747c499533",
+    "url": "/static/js/runtime-main.2a20d6e6.js"
+  }
+]);
