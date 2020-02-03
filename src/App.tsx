5c9d0b966301a78c1b2ccb3012a0389f370f510e
daxi <aljedaxi@gmail.com>
2020-02-03 00:13:25 -0700

    added help Button, Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TaskList} from './components/list';
+import {HelpModal, BottomButton} from './components/help';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const initialTasks = [{
 	name: ''
 }];
 
-const App: React.FC = () => (
-	<TaskList
-		initialTasks={initialTasks}
-	/>
-);
+const App: React.FC = () => {
+	const [show, setShow] = useState(false);
+	const handleCloseModal = () => setShow(false);
+	const handleOpenModal = () => setShow(true);
+	return (
+		<>
+			<HelpModal
+				show={show}
+				handleClose={handleCloseModal}
+				handleOpen={handleOpenModal}
+			/>
+			<TaskList
+				initialTasks={initialTasks}
+			/>
+			<BottomButton
+				onClick={handleOpenModal}
+			/>
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/help.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Modal, Button} from 'react-bootstrap';
+const DEFAULT_MESSAGE = 'Throw stuff you need to do onto the stack using the text box at the top. Finish stuff and take it off with DELETE DELETE DELETE.';
+
+export const HelpModal: React.FunctionComponent<{
+	helpMessage?: string;
+	show: boolean;
+	handleOpen: () => void;
+	handleClose: () => void;
+	// eslint-disable-next-line react/prop-types
+}> = ({helpMessage, show, handleOpen, handleClose}) => {
+	helpMessage = helpMessage || DEFAULT_MESSAGE;
+
+	return (
+		<Modal
+			show={show}
+			onClick={handleOpen}
+			onHide={handleClose}
+		>
+			<Modal.Header>
+				<Modal.Title>
+				Help
+				</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				{helpMessage}
+			</Modal.Body>
+		</Modal>
+	);
+};
+
+export const BottomButton: React.FunctionComponent<{
+	// eslint-disable-next-line react/prop-types
+	onClick: () => void;
+}> = ({onClick}) => (
+	<Button
+		onClick={onClick}
+		size='sm'
+		style={{
+			position: 'fixed',
+			bottom: 10,
+			left: '50%',
+			right: '50%',
+			marginLeft: '-50px',
+			width: '100px',
+		}}
+	>
+		Send Help
+	</Button>
+);
